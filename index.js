165bb19f34bea1ff31fa73a89a2f8d3bba7ab0cb
bethtechow <bethtechow@yahoo.com>
2017-11-23 14:27:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string) {
   {return ("I can't hear you!")}
   else if (string.toUpperCase() == string){
    return ("YES INDEED!")
-   elso if (string = "I love you, Grandma.") (
+   else if (string = "I love you, Grandma.") (
      return ("I love you, too.")
   }
 }
