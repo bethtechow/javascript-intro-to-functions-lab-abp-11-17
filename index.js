316c3f73bc94bf8b6414d07ade3780e80740059a
bethtechow <bethtechow@yahoo.com>
2017-11-23 13:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log (string.toLowerCase())
 }
+var lowercase = string
+  string.toLowerCase() === lowercase
+  if true
+    console.log ("I can't hear you!")
+
